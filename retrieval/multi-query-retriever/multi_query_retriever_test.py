02ad06270d7991ab8d0976b349ffadc6cf2878b1
jiyong.yoon <jyyoon0615@naver.com>
2024-06-23 00:17:32 +0900

    multi_query_retriever + 추가 테스트

--- retrieval/multi-query-retriever/multi_query_retriever_test.py
+++ retrieval/multi-query-retriever/multi_query_retriever_test.py
@@ -23,8 +23,9 @@ ko_embedding = HuggingFaceEmbeddings(
 vectordb = Chroma.from_documents(documents=splits, embedding=ko_embedding)
 
 
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
@@ -36,16 +37,59 @@ llm = ChatOpenAI(
     openai_api_key=os.environ['OPEN_API_KEY']
 )
 
+# Set logging for the queries
+import logging
+
+logging.basicConfig()
+logging.getLogger("langchain.chains.retrieval_qa").setLevel(logging.INFO)
+logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
+
+### Basic-Query-Retriever
+retriever_from_llm = RetrievalQA.from_llm(
+    retriever=vectordb.as_retriever(
+        # 아래와 같은 설정들을 해주지 않아도 기본적으로 여러개의 질문을 만들어서 답변을 받다보니, 좀 더 다양하고 구체적인 답변을 받을 수 있음
+        # search_type="mmr",
+        # search_kwargs={'k': 3, 'fetch_k': 10}
+    ),
+    llm=llm,
+    return_source_documents=True
+)
+
+unique_docs = retriever_from_llm(question)
+print(len(unique_docs))
+print(unique_docs)
+""" 프롬프팅 없는 일반 RetrievalQA-Query
+{
+    'query': '삼성전자 갤럭시 S24는 어떨 예정이야?', 
+    'result': '삼성전자 갤럭시 S24는 AI 서비스가 대거 탑재될 것으로 예상되며, 새로운 스마트폰 기준을 제시할 것으로 전망됩니다. 또한, 갤럭시 S24에는 실시간 통화 통역 기능이 포함될 것으로 알려졌습니다.', 
+    'source_documents': [
+        Document(
+            page_content="[서울=뉴시스] 삼성전자가 17일 오전 10시(현지시간, 한국 시간 18일 오전 3시) 미국 캘리포니아주 산호세(새너제이)에서 '삼성 갤럭시 언팩 2024'를 열고 갤럭시 S24를 공개한다. 사진은 포르투갈에서 유출된 갤럭시 S24 시리즈 포스터 추정 이미지 (사진=theonecid 엑스 캡처)  *재판매 및 DB 금지[서울=뉴시스]윤정민 기자 = 인공지능(AI) 서비스가 대거 탑재될 삼성전자 플래그십 스마트폰 '갤럭시 S24'가 18일 베일을 벗는다. 갤럭시 S23이 전작 대비 카메라, 디자인 등 대폭 개선됐다면, 이번 신작은", 
+            metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+        ), 
+        Document(
+            page_content="[서울=뉴시스] 삼성전자가 17일 오전 10시(현지시간, 한국 시간 18일 오전 3시) 미국 캘리포니아주 산호세(새너제이)에서 열 '삼성 갤럭시 언팩 2024'의 주제는 '모바일 AI의 새로운 시대 개막'이다. 앞서 삼성전자가 AI를 스마트폰 차기작 특징으로 예고했던 만큼 어떤 AI 기능이 실릴지 관심이 쏠린다.삼성전자가 공식적으로 밝힌 AI 서비스는 실시간 통화 통역이다. 이미 SK텔레콤 '에이닷' 등 통화 통역을 지원하는 앱이 있다. 하지만 자체 AI가 탑재될 갤럭시 S24는 별도 앱을 설치하지 않아도 통역 통화를 이용할 수", 
+            metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+        ), 
+        Document(
+            page_content='한층 진화된 AI 서비스로 새로운 스마트폰 기준을 제시할 것으로 전망된다.업계 모두가 스마트폰 시장 선두 주자인 삼성전자의 신작을 궁금해하는 만큼 공신력 있는 IT 팁스터(정보유출자)들이 여러 정보를 유출했다. 14일 현재까지 삼성전자가 공개한 스마트폰 신작 정보와 업계에 유출된 내용을 종합해 갤럭시 S24 예상 사양을 정리해 봤다."손안에 만능 비서 담았다"…인터넷 연결 없어도 AI 쓸 수 있는 \'온디바이스 AI폰\'',
+            metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+        ), 
+        Document(
+            page_content="[서울=뉴시스] 8일 업계에 따르면 삼성전자는 미국 삼성닷컴 새 스마트폰 사전예약 알림 창구에 '갤럭시 AI와 함께하는 줌이 온다(Zoom with Galaxy AI is coming)'을 주제로 한 영상을 게재했다. (영상=미국 삼성닷컴 캡처) *재판매 및 DB 금지 *재판매 및 DB 금지AI를 통해 사진 속 일부 물체 크기를 더 키울 수 있는 기능도 나올 가능성이 있다. 삼성전자는 미국 삼성닷컴 새 스마트폰 사전 예약 알림 창구에 '갤럭시 AI와 함께하는 줌이 온다'를 주제로 한 영상을 게재했다. 나인투파이브구글 등 해외 IT", 
+            metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+        )
+    ]
+}
+"""
+
+### Multi-Query-Retriever
+
 retriever_from_llm = MultiQueryRetriever.from_llm(
     retriever=vectordb.as_retriever(),
     llm=llm
 )
 
-# Set logging for the queries
-import logging
-
-logging.basicConfig()
-logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
 """ MultiQueryRetriever가 사용자 질문으로 재생성한 질문
 INFO:langchain.retrievers.multi_query:Generated queries: [
     '1. 삼성 갤럭시 S24 출시 일정은 언제인가요?', 
@@ -53,29 +97,43 @@ INFO:langchain.retrievers.multi_query:Generated queries: [
     '3. 삼성 갤럭시 S24의 가격대는 어느 정도일까요?'
 ]
 """
-
-unique_docs = retriever_from_llm.get_relevant_documents(query=question)
-print(len(unique_docs))
-print(unique_docs)
+#
+# unique_docs = retriever_from_llm.get_relevant_documents(query=question)
+# print(len(unique_docs))
+# print(unique_docs)
 """
 [
     Document(
         page_content="[서울=뉴시스] 삼성전자가 17일 오전 10시(현지시간, 한국 시간 18일 오전 3시) 미국 캘리포니아주 산호세(새너제이)에서 '삼성 갤럭시 언팩 2024'를 열고 갤럭시 S24를 공개한다. 사진은 포르투갈에서 유출된 갤럭시 S24 시리즈 포스터 추정 이미지 (사진=theonecid 엑스 캡처)  *재판매 및 DB 금지[서울=뉴시스]윤정민 기자 = 인공지능(AI) 서비스가 대거 탑재될 삼성전자 플래그십 스마트폰 '갤럭시 S24'가 18일 베일을 벗는다. 갤럭시 S23이 전작 대비 카메라, 디자인 등 대폭 개선됐다면, 이번 신작은", 
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
     Document(
         page_content="[서울=뉴시스] 삼성전자가 17일 오전 10시(현지시간, 한국 시간 18일 오전 3시) 미국 캘리포니아주 산호세(새너제이)에서 열 '삼성 갤럭시 언팩 2024'의 주제는 '모바일 AI의 새로운 시대 개막'이다. 앞서 삼성전자가 AI를 스마트폰 차기작 특징으로 예고했던 만큼 어떤 AI 기능이 실릴지 관심이 쏠린다.삼성전자가 공식적으로 밝힌 AI 서비스는 실시간 통화 통역이다. 이미 SK텔레콤 '에이닷' 등 통화 통역을 지원하는 앱이 있다. 하지만 자체 AI가 탑재될 갤럭시 S24는 별도 앱을 설치하지 않아도 통역 통화를 이용할 수", 
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
     Document(
         page_content="[서울=뉴시스] 8일 업계에 따르면 삼성전자는 미국 삼성닷컴 새 스마트폰 사전예약 알림 창구에 '갤럭시 AI와 함께하는 줌이 온다(Zoom with Galaxy AI is coming)'을 주제로 한 영상을 게재했다. (영상=미국 삼성닷컴 캡처) *재판매 및 DB 금지 *재판매 및 DB 금지AI를 통해 사진 속 일부 물체 크기를 더 키울 수 있는 기능도 나올 가능성이 있다. 삼성전자는 미국 삼성닷컴 새 스마트폰 사전 예약 알림 창구에 '갤럭시 AI와 함께하는 줌이 온다'를 주제로 한 영상을 게재했다. 나인투파이브구글 등 해외 IT", 
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
     Document(
         page_content='닫기\n\n\n \n\n글자 크기 변경하기\n\n\n\n가1단계\n작게\n\n\n가2단계\n보통\n\n\n가3단계\n크게\n\n\n가4단계\n아주크게\n\n\n가5단계\n최대크게\n\n\n\n\n\n\nSNS 보내기\n\n\n\n인쇄하기\n\n\n\n\n\n\n\n\n삼성전자, 17일(美 현지시간) 언팩서 갤럭시 S24 시리즈 공개국내 출고가 일반·플러스 동결, 울트라 약 10만원 인상 전망실시간 통화 통역, 이미지 자동 편집 등 AI 기능 탑재 예상',
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
     Document(
         page_content='[서울=뉴시스] 삼성전자 갤럭시 S24 시리즈 예상 색상 유출본 (사진=인터넷 커뮤니티 캡처) *재판매 및 DB 금지외관의 경우 갤럭시 S24 울트라 모델이 전작과 달리 티타늄 소재를 썼다는 데 눈에 띈다. 티타늄은 알루미늄보다 무게가 무겁지만 내구성이 강하다.스마트폰 두뇌 역할을 하는 모바일 애플리케이션 프로세서(AP)에는 퀄컴 스냅드래곤8 3세대와 삼성 엑시노스 2400이 적용될 것으로 보인다. 국내 시장에 출시될 제품의 경우 퀄컴 칩은 울트라 모델에만 탑재되고 일반과 플러스 모델에는 엑시노스 칩이 실릴 전망이다.작업 처리에', 
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
     Document(
         page_content='한층 진화된 AI 서비스로 새로운 스마트폰 기준을 제시할 것으로 전망된다.업계 모두가 스마트폰 시장 선두 주자인 삼성전자의 신작을 궁금해하는 만큼 공신력 있는 IT 팁스터(정보유출자)들이 여러 정보를 유출했다. 14일 현재까지 삼성전자가 공개한 스마트폰 신작 정보와 업계에 유출된 내용을 종합해 갤럭시 S24 예상 사양을 정리해 봤다."손안에 만능 비서 담았다"…인터넷 연결 없어도 AI 쓸 수 있는 \'온디바이스 AI폰\'', 
-        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), 
-    Document(page_content='기준 오닉스 블랙, 마블 그레이, 코발트 바이올렛, 앰버 옐로우 등 4가지이며 울트라는 티타늄 블랙, 티타늄 그레이, 티타늄 바이올렛, 티타늄 옐로우 등 4가지가 될 것으로 예상된다. 이 밖에 삼성닷컴 판매 한정으로 티타늄 그린, 티타늄 블루, 티타늄 오렌지도 추가될 전망이다.국내 출고가 일반·플러스 동결, 울트라 약 10만원 인상될 듯…19일 사전예약소비자 입장에서 제일 궁금한 건 가격과 사전 예약 혜택이다.일반형, 플러스 모델(256GB 기준) 가격은 전작과 같을 것으로 전망된다. 256GB 용량 기준 일반형과 플러스 모델', metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}), Document(page_content='예상 출고가는 각각 115만5000원, 135만3000원으로 잠정 책정된 것으로 알려졌다. 512GB의 경우 전작 대비 2만2000원 비싸진 129만8000원, 149만6000원이 될 전망이다.울트라 모델은 전작보다 비싸질 것으로 알려졌다. 256GB는 9만9000원 오른 169만8400원, 512GB는 12만1000원 오른 184만1400원으로 예상된다.사전 예약 기간은 19일부터 25일까지 7일간 진행될 것으로 예상되며 사전 예약 혜택은 더블 스토리지, 갤럭시 워치 할인, 갤럭시 버즈 FE 할인 등이 거론되고 있다.더블', metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"})]
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
+    Document(
+        page_content='기준 오닉스 블랙, 마블 그레이, 코발트 바이올렛, 앰버 옐로우 등 4가지이며 울트라는 티타늄 블랙, 티타늄 그레이, 티타늄 바이올렛, 티타늄 옐로우 등 4가지가 될 것으로 예상된다. 이 밖에 삼성닷컴 판매 한정으로 티타늄 그린, 티타늄 블루, 티타늄 오렌지도 추가될 전망이다.국내 출고가 일반·플러스 동결, 울트라 약 10만원 인상될 듯…19일 사전예약소비자 입장에서 제일 궁금한 건 가격과 사전 예약 혜택이다.일반형, 플러스 모델(256GB 기준) 가격은 전작과 같을 것으로 전망된다. 256GB 용량 기준 일반형과 플러스 모델', 
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    ), 
+    Document(
+        page_content='예상 출고가는 각각 115만5000원, 135만3000원으로 잠정 책정된 것으로 알려졌다. 512GB의 경우 전작 대비 2만2000원 비싸진 129만8000원, 149만6000원이 될 전망이다.울트라 모델은 전작보다 비싸질 것으로 알려졌다. 256GB는 9만9000원 오른 169만8400원, 512GB는 12만1000원 오른 184만1400원으로 예상된다.사전 예약 기간은 19일부터 25일까지 7일간 진행될 것으로 예상되며 사전 예약 혜택은 더블 스토리지, 갤럭시 워치 할인, 갤럭시 버즈 FE 할인 등이 거론되고 있다.더블',
+        metadata={'language': 'ko', 'source': 'https://n.news.naver.com/mnews/article/003/0012317114?sid=105', 'title': "언팩 D-4, 세계 최초 AI폰 '갤S24' 이렇게 나온다"}
+    )
+]
 """
